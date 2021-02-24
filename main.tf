5e581a4f4aba3a745e74ba440e1183bf4dc805e2
tomarv2 <varuntomar2019@gmail.com>
2021-02-24 07:45:20 -0800

    moving version to seperate file

--- main.tf
+++ main.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_version            = ">= 0.14"
-  required_providers {
-    azurerm = {
-      version                     = "~> 2.48"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  # NOTE:  I noticed an issue with getting access to Azure even after setting env.
-  # variables, adding it to mitigate auth issues.
-  subscription_id             = var.subscription_id
-  client_id                   = var.client_id
-  client_secret               = var.client_secret
-  tenant_id                   = var.tenant_id
-}
-
 locals {
   shared_tags = map(
       "Name", "${var.teamid}-${var.prjid}",
