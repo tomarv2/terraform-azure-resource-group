7e4f8bdd4ca6702233e66e0ffc6e1dfd106aa945
tomarv2 <varuntomar2019@gmail.com>
2022-09-09 14:51:00 -0700

    test

--- providers.tf
+++ providers.tf
@@ -2,11 +2,7 @@ terraform {
   required_version = ">= 1.0.1"
   required_providers {
     azurerm = {
-<<<<<<< HEAD:providers.tf
       version = "~> 3.21.1"
-=======
-      version = "~> 2.98"
->>>>>>> 246f8799937f128a6d2f704c90389ae598fcc16d:versions.tf
     }
   }
 }
