860d337f37c08ea548c2341505772144c32881ee
tomarv2 <varuntomar2019@gmail.com>
2022-09-09 14:50:28 -0700

    test

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -3,11 +3,3 @@ output "resource_group_id" {
   value       = module.resource_group.resource_group_id
 }
 
-output "resource_group_name" {
-<<<<<<< HEAD:examples/outputs.tf
-  description = "Name of the Resource group"
-=======
-  description = "Name of the resource group"
->>>>>>> 246f8799937f128a6d2f704c90389ae598fcc16d:examples/sample/output.tf
-  value       = module.resource_group.resource_group_name
-}
